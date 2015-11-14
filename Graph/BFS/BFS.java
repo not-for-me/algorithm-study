028967af80cf7462a5b579dc8ea2b5e941849f42
조우진(Mac) <woojin@tmon.co.kr>
2015-11-14 18:52:25 +0900

    update BFS Java version

--- Graph/BFS/BFS.java
+++ Graph/BFS/BFS.java
@@ -1,11 +1,84 @@
-import java.io.*;
 import java.util.*;
+import java.util.stream.IntStream;
 
 /**
- *
+ * Problem Ref.: https://www.hackerrank.com/challenges/bfsshortreach/copy-from/15190166
+ * Data Structure
+ *  @List<Vertex> vertexes: Total vertex list
+ *  @Map<Integer, Set<Vertex>> vertexMap: Adjacency information of each vertex
  **/
 public class BFS {
-    private Enum VertexStatusEnum {
+    public static void main(String[] args) {
+        new BFS().solve();
+    }
+
+    private void solve() {
+        Scanner sc = new Scanner(System.in);
+        int cases = Integer.parseInt(sc.nextLine());
+        while (cases-- > 0) {
+            final String[] params = sc.nextLine().split(" ");
+            final int vertexCnt = Integer.parseInt(params[0]);
+            final int edgeCnt = Integer.parseInt(params[1]);
+
+            // 1. Create Graph Data Structure with HashMap
+            List<Vertex> vertexes = new ArrayList<>();
+            Map<Integer, Set<Vertex>> vertexMap = new HashMap<>();
+
+            vertexes.add(new Vertex(0)); // dummy Vertex
+            IntStream.range(0, vertexCnt).forEach(id -> {
+                vertexes.add(new Vertex(id + 1));
+                vertexMap.put(id + 1, new HashSet<>());
+            });
+
+            // 2. Read and store edge information.
+            IntStream.range(0, edgeCnt).forEach(x -> {
+                final String[] edgeInfo = sc.nextLine().split(" ");
+                final int fromVertex = Integer.parseInt(edgeInfo[0]);
+                final int toVertex = Integer.parseInt(edgeInfo[1]);
+                Set<Vertex> connectedSet = vertexMap.get(fromVertex);
+                connectedSet.add(vertexes.get(toVertex));
+                vertexMap.put(fromVertex, connectedSet);
+
+                connectedSet = vertexMap.get(toVertex);
+                connectedSet.add(vertexes.get(fromVertex));
+                vertexMap.put(toVertex, connectedSet);
+            });
+
+            // 3. Read Source vertex and Explore all vertexes with BFS.
+            final int sourceVertexId = Integer.parseInt(sc.nextLine());
+            Vertex sourceVertex = vertexes.get(sourceVertexId);
+            sourceVertex.setStatus(VertexStatusEnum.GREY);
+            sourceVertex.setDistance(0);
+            sourceVertex.setPredecessor(null);
+
+            Queue<Vertex> queue = new LinkedList<>();
+            queue.add(sourceVertex);
+            while (queue.size() > 0) {
+                Vertex vertex = queue.poll();
+                Set<Vertex> connectedVertexes = vertexMap.get(vertex.getId());
+                connectedVertexes.forEach(v -> {
+                    if (v.getStatus() == VertexStatusEnum.WHITE) {
+                        v.setStatus(VertexStatusEnum.GREY);
+                        v.setDistance(vertex.getDistance() + 6);
+                        v.setPredecessor(vertex);
+                        queue.add(v);
+                    }
+                });
+
+                vertex.setStatus(VertexStatusEnum.BLACK);
+            }
+
+            for(int i = 1; i <= vertexCnt; i++ ) {
+                Vertex vertex =  vertexes.get(i);
+                if( vertex.getDistance() != 0) {
+                    System.out.print(vertex.getDistance() + " ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    private enum VertexStatusEnum {
         WHITE, GREY, BLACK
     }
 
@@ -29,20 +102,28 @@ public class BFS {
         public void setId(int id) {
             this.id = id;
         }
-        
+
+        public VertexStatusEnum getStatus() {
+            return status;
+        }
+
+        public void setStatus(VertexStatusEnum status) {
+            this.status = status;
+        }
+
         public int getDistance() {
             return distance;
         }
-        
+
         public void setDistance(int distance) {
             this.distance = distance;
         }
-        
-        public Vertex getVertex() {
+
+        public Vertex getPredecessor() {
             return predecessor;
         }
-        
-        public void setVertex(Vertext predecessor) {
+
+        public void setPredecessor(Vertex predecessor) {
             this.predecessor = predecessor;
         }
 
@@ -51,69 +132,13 @@ public class BFS {
             if (getClass() != obj.getClass())
                 return false;
 
-            Vertex other = (Vertext) obj;
+            Vertex other = (Vertex) obj;
             return this.id == other.id;
         }
 
         @Override
         public int hashCode() {
             return this.id;
-        }
-    }
-
-    public static void main(String[] args) {
-        new BFS().solve();
-    }
-
-    private void solve() {
-    	Scanner sc = new Scanner(System.in);
-        int cases = sc.nextInt();
-        while ( cases-- ) {
-            final int[] params = sc.nextLine().split(" ");
-            final int vertexCnt = params[0];
-            final int edgeCnt = params[1];
-
-            // 1. Create Vertex Objects.
-            int vertexId = 0;
-            List<Vertex> vertexes = new ArrayList<>();
-            while ( vertexId < vertexCnt ) {
-                vertexes.add( vertexId++, new Vertex(vertexId) );
-            }
-
-            // 2. Create a set for store adjacent vertexes of a specific vertex.
-            List<Set<Vertex>> vertexList = new ArrayList<>();
-            int vertexPos = 0;
-            while (vertexPos < vertexCnt) {
-                vertexList.add(vertexPos++, new HashSet<Vertex>());
-            }
-
-            // 3. Read and store edge information.
-            int inputEdgeNum = 0;
-            while ( inputEdgeNum++ < edgeCnt ) {
-                final int[] edgeInfo = sc.nextLine().split(" ");
-                final int fromVertex = edgeInfo[0];
-                final int toVertex = edgeInfo[1];
-                vertexList.get(fromVertex).add(vertexes.get(toVertex));
-                vertexList.get(toVertex).add(vertexes.get(fromVertext));
-            }
-
-            // 4. Read Source vertex.
-            final int sourceVertexId = sc.nextInt();
-
-            // 5. Explore all vertexes with BFS.
-            Queue<Vertex> queue = new LinkedList<>();
-            vertexPos = 0;
-            queue.add(vertexList.get(vertexPos));
-
-            while ( vertexPos < vertexCnt ) {
-                Vertex vertex = queue.poll();
-                Set<Vertex> connectedVertexes = vertexList.get(vertex.getId());
-                if ( connectedVertexes.size() > 0 ) {
-                    for (Vertex vertex : connectedVertexes. )
-                }
-
-                vertexPos++;
-            }
         }
     }
 }
